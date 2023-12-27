cab128df408b836b268471588fde5d27b65221a9
rahulas2255 <rahulayyappath@gmail.com>
2023-12-27 12:12:30 +0530

    final stage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Counter from './Components/Counter'
+
+function App() {
+  
+
+  return (
+    <div style={{height:'100vh'}} className='w-100 d-flex justify-content-center align-items-center'>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
